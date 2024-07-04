f5249a71300d6f98c9d975721e83bbf5cb9928d8
Zedai00 <33319711+Zedai00@users.noreply.github.com>
2024-07-04 17:28:50 +0530

    Add New Button

--- script.js
+++ script.js
@@ -11,14 +11,40 @@ function createGrid(size) {
   }
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let opacity = 1
+
 function addEvent() {
   let pixels = document.querySelectorAll('.pixel')
   pixels.forEach((elm) => {
     elm.addEventListener('mouseover', () => {
-      elm.style.background = 'blue'
+      let r = getRandomInt(255)
+      let g = getRandomInt(255)
+      let b = getRandomInt(255)
+      elm.style.background = `rgba(${r}, ${g}, ${b}, ${opacity})`
+      if (opacity < 0) {
+        opacity = 1
+      } else {
+        opacity = (opacity - 0.1)
+      }
     })
   })
 }
 
-createGrid(50)
+function promptGrid() {
+  let size = parseInt(prompt("Enter Grid Size (Max: 100): "))
+  createGrid(size)
+}
+
+let newSize = document.querySelector(".button")
+newSize.addEventListener('click', () => {
+  container.innerHTML = ''
+  promptGrid()
+  addEvent()
+})
+
+promptGrid()
 addEvent()
